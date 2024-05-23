429930a991867a56da6b003f881ea4e42681ec2a
krazykrimz <krazy.krimz@gmail.com>
2024-05-23 09:46:07 -0500

    fixed css

--- source/main.js
+++ source/main.js
@@ -1,9 +1,9 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
-import '../styles/header.css';
-import '../styles/hero.css';
-import '../styles/herobanner.css';
-import '../styles/funeral.css';
-import '../styles/gallery.css';
+import '../cssStyles/header.css';
+import '../cssStyles/hero.css';
+import '../cssStyles/herobanner.css';
+import '../cssStyles/funeral.css';
+import '../cssStyles/gallery.css';
 import '../styles/utils.css';
 
