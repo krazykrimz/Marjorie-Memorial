77358aa20eb5431ba9e6e69aaf1a644539e558b5
krazykrimz <krazy.krimz@gmail.com>
2024-05-22 23:33:25 -0500

    first commit

---
+++ source/main.js
@@ -0,0 +1,9 @@
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/header.css';
+import '../styles/hero.css';
+import '../styles/herobanner.css';
+import '../styles/funeral.css';
+import '../styles/gallery.css';
+import '../styles/utils.css';
+
